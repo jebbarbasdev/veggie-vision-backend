d6e67fb073e25e7c44b4ea37cd31242514daf7ec
Jesús Barboza <enriquebarbozajeb@gmail.com>
2025-05-25 22:37:02 -0700

    feat: add cors and update model

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from endpoints import router
 from config import API_HOST, API_PORT
 import uvicorn
 
 app = FastAPI(title="Veggie Vision API")
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Include the router with the ambient endpoint
 app.include_router(router)
